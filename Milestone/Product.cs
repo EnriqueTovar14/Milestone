6278e071675f8ef81fdb0e4d8a4eb97460a7909d
enrique tovar <enriquektovar14@gmail.com>
2019-03-10 21:17:14 -0700

    comit

---
+++ Milestone/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// Enrique Tovar
+//This is my own work
+namespace Milestone
+{
+    class Product
+    {
+        private String name;
+        private String id;
+        private String origin;
+        private double weight;
+        private double price;
+        private String brand;
+        private int stock;
+
+        public Product(string n, string i, string o, double w, double p, string b, int s)
+        {
+            name = n;
+            id = i;
+            origin = o;
+            weight = w;
+            price = p;
+            brand = b;
+            stock = s;
+        }
+        public string getName() { return name; }
+        public string getId() { return id; }
+        public string getOrigin() { return origin; }
+        public double getWeight() { return weight; }
+        public double getPrice() { return price; }
+        public string getBrand() { return brand; }
+        public int getStock() { return stock; }
+
+        public void setName(string n) { name = n; }
+        public void setId(string i) { id = i; }
+        public void setOrigin(string o) { origin = o; }
+        public void setWeight(double w) { weight = w; }
+        public void setPrice(double p) { price = p; }
+        public void setBrand(string b) { brand = b; }
+        public void setStock (int s) { stock = s; }
+
+
+    }
+}
--- Milestone/Program.cs
+++ Milestone/Program.cs
@@ -10,6 +10,62 @@ namespace Milestone
     {
         static void Main(string[] args)
         {
+            Product Apple = new Product("Honeycrisp", "1", "Oregon", .05, 2.00,"Tovar", 1000);
+            //name test
+            Console.WriteLine("Apple name is " + Apple.getName());
+
+            Console.WriteLine("Setting apple name to Green");
+            Apple.setName("Green");
+
+            Console.WriteLine("Apple name is " + Apple.getName());
+
+            //id test
+            Console.WriteLine("Apple ID is " + Apple.getId());
+
+            Console.WriteLine("Setting apple Id to 2");
+            Apple.setId("2");
+
+            Console.WriteLine("Apple ID is " + Apple.getId());
+
+            //origin test
+            Console.WriteLine("Apple origin is " + Apple.getOrigin());
+
+            Console.WriteLine("Setting apple origin to Arizona");
+            Apple.setOrigin("Arizona");
+
+            Console.WriteLine("Apple origin is " + Apple.getOrigin());
+
+            //weight test
+            Console.WriteLine("Apple weight is " + Apple.getWeight());
+
+            Console.WriteLine("Setting apple weight to 2.5");
+            Apple.setWeight(2.5);
+
+            Console.WriteLine("Apple weight is " + Apple.getWeight());
+
+            //price test
+            Console.WriteLine("Apple price is " + Apple.getPrice());
+
+            Console.WriteLine("Setting apple price to 3.00");
+            Apple.setPrice(3.00);
+
+            Console.WriteLine("Apple price is " + Apple.getPrice());
+
+            //brand test
+            Console.WriteLine("Apple brand is " + Apple.getBrand());
+
+            Console.WriteLine("Setting apple brand to Lopes");
+            Apple.setBrand("Lopes");
+
+            Console.WriteLine("Apple brand is " + Apple.getBrand());
+
+            //stock test
+            Console.WriteLine("Apple stock is " + Apple.getStock());
+
+            Console.WriteLine("Setting apple stock to 300");
+            Apple.setStock(300);
+
+            Console.WriteLine("Apple stock is " + Apple.getStock());
         }
     }
 }
